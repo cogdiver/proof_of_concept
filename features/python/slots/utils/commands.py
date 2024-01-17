138116d227443fa4b73f3b52b62bf8e562ed1b35
cogdiver <valentina.arenas@xertica.com>
2024-01-17 17:25:54 -0500

    git add utils functions to cli commands and prints formated

---
+++ features/python/slots/utils/commands.py
@@ -0,0 +1,9 @@
+from tests.advantages import usage, faster_access, memory_saving, secure_access
+
+
+commands = {
+    "usage": usage,
+    "faster_access": faster_access,
+    "memory_saving": memory_saving,
+    "secure_access": secure_access,
+}
