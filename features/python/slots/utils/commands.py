e4e15cfd759d9ccf7973835a1cb051057a45ba7e
cogdiver <valentina.arenas@xertica.com>
2024-01-17 20:46:13 -0500

    add drawbacks functions

--- features/python/slots/utils/commands.py
+++ features/python/slots/utils/commands.py
@@ -1,4 +1,5 @@
 from tests.advantages import usage, faster_access, memory_saving, secure_access
+from tests.drawbacks import slots_as_list, weakref_access, dict_access, inheritance
 
 
 commands = {
@@ -6,4 +7,8 @@ commands = {
     "faster_access": faster_access,
     "memory_saving": memory_saving,
     "secure_access": secure_access,
+    "slots_as_list": slots_as_list,
+    "weakref_access": weakref_access,
+    "dict_access": dict_access,
+    "inheritance": inheritance,
 }
