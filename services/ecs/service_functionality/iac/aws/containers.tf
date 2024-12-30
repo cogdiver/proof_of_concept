fea82ea109be39ef6d64481e15069c27af257ff2
cogdiver <valentinaarenas2@gmail.com>
2024-12-30 17:33:23 -0500

    add service as IaC

--- services/ecs/service_functionality/iac/aws/containers.tf
+++ services/ecs/service_functionality/iac/aws/containers.tf
@@ -24,3 +24,25 @@ resource "aws_ecs_task_definition" "task" {
     service_name = var.base_name
   })
 }
+
+resource "aws_ecs_service" "service" {
+  depends_on = [aws_lb_listener.listener]
+
+  name            = "${var.base_name}_service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 0
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "${var.base_name}_app"
+    container_port   = 8080
+  }
+}
