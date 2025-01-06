5368bb51819dcfb656c2562e124158318e5da0e0
cogdiver <valentinaarenas2@gmail.com>
2025-01-06 11:06:59 -0500

    update API

--- services/ecs/service_functionality/app/main.py
+++ services/ecs/service_functionality/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import socket
+import time
+import psutil
 
 app = FastAPI()
 
@@ -18,4 +20,23 @@ def get_ip():
 def health():
     return {
         "status": "ok"
+    }
+
+@app.get("/simulate-cpu")
+def simulate_cpu():
+    cpu_utilization = psutil.cpu_percent(interval=1)
+    fake_list = []
+
+    while cpu_utilization < 50:
+        try:
+            print(cpu_utilization)
+            fake_list.extend([x**2 for x in range(1000000)])
+            cpu_utilization = psutil.cpu_percent(interval=1)
+        except Exception as e:
+            print(e)
+            break
+
+    return {
+        "status": "CPU utilization simulated",
+        "cpu_utilization": cpu_utilization
     }
